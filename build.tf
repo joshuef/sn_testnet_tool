cfd6b9bd58a60161e68d04f7d3d8e98c1f12352b
Josh Wilson <joshuef@gmail.com>
2021-09-30 17:10:52 +0200

    goes...but long

--- build.tf
+++ build.tf
@@ -42,19 +42,64 @@ resource "digitalocean_droplet" "node_builder" {
             EOT
             ,
             "apt -qq install musl-tools -y ",
+            "sudo apt install apt-transport-https ca-certificates curl software-properties-common",
+            #docker setup
+            "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+            "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",
+            "apt-cache policy docker-ce",
+            "sudo apt install docker-ce",
+            "sudo groupadd docker",
+            "sudo usermod -aG docker $USER",
+            #rustup
             "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q --default-host x86_64-unknown-linux-gnu --default-toolchain stable --profile minimal -y",
             ". $HOME/.cargo/env",
-            "rustup target add x86_64-unknown-linux-musl",
-            "cargo -q build --release --target=x86_64-unknown-linux-musl",
+
+            # start docker
+            "sudo systemctl start docker",
+            "cargo install cross",
+            # "rustup target add x86_64-unknown-linux-musl",
+            # "cargo -q build --release --target=x86_64-unknown-linux-musl",
+        ]
+    }
+
+    # provisioner "local-exec" {
+    #     command = <<EOH
+    #         mkdir -p ~/.ssh/
+    #         touch ~/.ssh/known_hosts
+    #         ssh-keyscan -H ${self.ipv4_address} >> ~/.ssh/known_hosts
+    #         rsync root@${self.ipv4_address}:/root/safe_network/target/x86_64-unknown-linux-musl/release/sn_node ${var.working_dir}
+    #     EOH
+    # }
+
+    provisioner "remote-exec" {
+        inline = [
+            "cd safe_network",
+            ". $HOME/.cargo/env",
+            "rustup target add x86_64-pc-windows-msvc",
+            # "rustup toolchain install stable-x86_64-pc-windows-gnu",
+            "cross build --release --target x86_64-pc-windows-msvc"
         ]
     }
 
     provisioner "local-exec" {
         command = <<EOH
-            mkdir -p ~/.ssh/
-            touch ~/.ssh/known_hosts
-            ssh-keyscan -H ${self.ipv4_address} >> ~/.ssh/known_hosts
-            rsync root@${self.ipv4_address}:/root/safe_network/target/x86_64-unknown-linux-musl/release/sn_node ${var.working_dir}
+            rsync root@${self.ipv4_address}:/root/safe_network/target/x86_64-pc-windows-msvc/release/sn_node.exe ${var.working_dir}/win_sn_node.exe
+        EOH
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "cd safe_network",
+            ". $HOME/.cargo/env",
+            # "rustup toolchain install stable-x86_64-apple-darwin",
+            "rustup target add x86_64-apple-darwin",
+            "cross build --release --target x86_64-apple-darwin"
+        ]
+    }
+    
+    provisioner "local-exec" {
+        command = <<EOH
+            rsync root@${self.ipv4_address}:/root/safe_network/target/x86_64-apple-darwin/release/sn_node ${var.working_dir}/mac_sn_node
         EOH
     }
 }
